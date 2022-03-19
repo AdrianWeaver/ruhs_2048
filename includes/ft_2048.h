7945d332bbca7d6e3571cfa0b3830fd2abbd8376
Adrian WEAVER <aweaver@student.42.fr>
2022-03-19 13:36:28 +0100

    adding define for keys + norminetting and pimping

--- includes/ft_2048.h
+++ includes/ft_2048.h
@@ -6,7 +6,7 @@
 /*   By: aweaver <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 09:38:39 by aweaver           #+#    #+#             */
-/*   Updated: 2022/03/19 12:05:00 by douattar         ###   ########.fr       */
+/*   Updated: 2022/03/19 13:35:38 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,21 +24,34 @@
 # include "libftprintf.h"
 # include "get_next_line.h"
 
+/****************************************************************************/
+/*								Data define									*/
+/****************************************************************************/
+
 typedef struct s_block {
 	int	number;
 	int	fusion;
 }		t_block;
-
-int	get_random(void);
-void	new_block(t_block *plate, int size);
-int	winning(t_block *plate, int size);
-int	is_power_2(int n);
-t_block	*initialisation(int size);
 
 enum	e_const
 {
 	WIN_VALUE = 2048
 };
 
+/****************************************************************************/
+/*							Functions define								*/
+/****************************************************************************/
+
+int		get_random(void);
+void	new_block(t_block *plate, int size);
+int		winning(t_block *plate, int size);
+int		is_power_2(int n);
+t_block	*initialisation(int size);
+
+/****************************************************************************/
+/*								Key define									*/
+/****************************************************************************/
+
+# define KEY_ESC 27
 
 #endif
