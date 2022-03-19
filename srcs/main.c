a2cd95dbe57a4e309926cf06c252644f06410b1d
Adrian WEAVER <aweaver@student.42.fr>
2022-03-19 19:33:37 +0100

    fixing display, need to work on resize

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,13 @@
 /*   By: aweaver <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 09:35:55 by aweaver           #+#    #+#             */
-/*   Updated: 2022/03/19 17:13:18 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/03/19 19:32:36 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_2048.h"
+
+int	g_resize = 0;
 
 void	ft_destroy_board(void *window, void **board, int size)
 {
@@ -31,6 +33,10 @@ void	**ft_create_box(void *window, int size, t_block *board_values)
 	int		tile_nb;
 	int		i;
 	int		j;
+	int		win_col;
+	int		win_line;
+	int		tile_length;
+	int		tile_height;
 
 	i = 0;
 	tile_nb = 0;
@@ -41,17 +47,21 @@ void	**ft_create_box(void *window, int size, t_block *board_values)
 		endwin();
 		exit (MALLOC_ERROR);
 	}
-	wresize(window, 2 + (size * 6), 4 + (size * 12));
+	getmaxyx(stdscr, win_line, win_col);
+	tile_length = (win_col - 4) / size;
+	tile_height = (win_line - 2) / size;
+	wresize(window, 2 + (size * tile_height), 4 + (size * tile_length));
 	wborder(window, 0, 0, 0, 0, 0, 0, 0, 0);
 	while (i < size)
 	{
 		j = 0;
 		while (j < size)
 		{
-			board[tile_nb] = subwin(window, 6, 12, 1 + (j * 6), 2 + (i * 12));
+			//board[tile_nb] = subwin(window, 6, ((win_col - 4) / size), 1 + (j * 6), 2 + (i * ((win_col - 4) / size)));
+			board[tile_nb] = subwin(window, tile_height, tile_length , 1 + (j * tile_height), 2 + (i *tile_length));
 			wborder(board[tile_nb], 0, 0, 0, 0, 0, 0, 0, 0);
 			mvwprintw(board[tile_nb],
-					3, 1, "%d", board_values[tile_nb].number);
+					(tile_height / 2), (tile_length /2), "%d", board_values[tile_nb].number);
 			tile_nb++;
 			j++;
 		}
@@ -71,16 +81,28 @@ void	ft_check_size(void *window, void **board, int size)
 	int	win_col;
 	int	win_line;
 
-	getmaxyx((WINDOW *)window, win_line, win_col);
-	while (win_col < 4 + (size * 12) || win_line < 2 + (size * 6))
+	getmaxyx(stdscr, win_line, win_col);
+	if (win_col < 4 + (size * 12) || win_line < 2 + (size * 6))
+	//while (win_col < 4 + (size * 12) || win_line < 2 + (size * 6))
 	{
-		getmaxyx((WINDOW *)window, win_line, win_col);
+		refresh();
+		//getmaxyx(stdscr, win_line, win_col);
 		ft_destroy_board(window, board, size);
+		clear();
 		ft_printf("Incorrect terminal size, please resize to minimum %dL %dH\n\r",
 			4 + (size * 6), 2 + (size * 12));
+		g_resize = 0;
 	}
+	while (g_resize == 0)
+		;
 }
 
+void	ft_resize_handle(int signal)
+{
+	(void)signal;
+	g_resize = 1;
+}
+	
 int	main(int argc, char **argv)
 {
 	t_block	*board_values;
@@ -104,9 +126,15 @@ int	main(int argc, char **argv)
 	noecho();
 	keypad(window, TRUE);
 	nodelay(window, TRUE);
+	signal(28, ft_resize_handle);
 	board = ft_create_box(window, size, board_values);
 	while (1)
 	{
+		if (g_resize == 1)
+		{
+			ft_check_size(window, board, size);
+			g_resize = 0;
+		}
 		key = wgetch(window);
 		if (key == KEY_UP || key == KEY_DOWN || key == KEY_RIGHT || key == KEY_LEFT)
 		{
@@ -118,7 +146,6 @@ int	main(int argc, char **argv)
 			break ;
 		}
 		refresh();
-		ft_check_size(window, board, size);
 		if (win == 1)
 			won = 1;
 	}
