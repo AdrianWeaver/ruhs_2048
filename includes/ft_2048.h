b2c61b88abed9c654ffc65d2e738d182d91de3f7
Adrian WEAVER <aweaver@student.42.fr>
2022-03-19 11:35:51 +0100

    adding curses.h

--- includes/ft_2048.h
+++ includes/ft_2048.h
@@ -6,7 +6,7 @@
 /*   By: aweaver <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 09:38:39 by aweaver           #+#    #+#             */
-/*   Updated: 2022/03/19 09:39:53 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/03/19 11:35:14 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <time.h>
 # include <stdlib.h>
 # include <signal.h>
+# include <curses.h>
 # include "libft.h"
 # include "libftprintf.h"
 # include "get_next_line.h"
