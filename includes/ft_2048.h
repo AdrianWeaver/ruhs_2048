75889db3d61bb51286ae33398fa60e68f5693960
Dassy OUATTARA <douattar@e2r4p12.clusters.42paris.fr>
2022-03-19 10:46:48 +0100

    block

--- includes/ft_2048.h
+++ includes/ft_2048.h
@@ -6,18 +6,31 @@
 /*   By: aweaver <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 09:38:39 by aweaver           #+#    #+#             */
-/*   Updated: 2022/03/19 09:39:53 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/03/19 10:36:56 by douattar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_2048_H
 # define FT_2048_H
+# define TRUE 1
+# define HOLLOW 0
+# define FALSE 0
 
 # include <time.h>
 # include <stdlib.h>
 # include <signal.h>
-# include "libft.h"
-# include "libftprintf.h"
-# include "get_next_line.h"
+# include "../libft/includes/libft.h"
+//# include "libftprintf.h"
+//# include "get_next_line.h"
+
+enum	e_const
+{
+	WIN_VALUE = 2048
+};
+
+typedef struct s_block {
+	int	number;
+	int	fusion;
+}		t_block;
 
 #endif
