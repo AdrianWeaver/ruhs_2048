b800a22e2508f54144aa0326a71ad8831cb5505e
aweaver <aweaver@student.42.fr>
2022-03-20 08:22:40 +0100

    adding better colors

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 09:35:55 by aweaver           #+#    #+#             */
-/*   Updated: 2022/03/20 07:22:00 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/03/20 08:10:14 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,48 @@ void	ft_destroy_board(void *window, void **board, int size)
 		i++;
 	}
 	delwin(window);
+}
+
+void	ft_color_border(t_block *board_values, void **board, int tile_nb)
+{
+	if (board_values[tile_nb].number == 2)
+		wattron(board[tile_nb], COLOR_PAIR(1));
+	if (board_values[tile_nb].number == 4)
+		wattron(board[tile_nb], COLOR_PAIR(2));
+	if (board_values[tile_nb].number == 8)
+		wattron(board[tile_nb], COLOR_PAIR(3));
+	if (board_values[tile_nb].number == 16)
+		wattron(board[tile_nb], COLOR_PAIR(4));
+	if (board_values[tile_nb].number == 32)
+		wattron(board[tile_nb], COLOR_PAIR(5));
+	if (board_values[tile_nb].number == 64)
+		wattron(board[tile_nb], COLOR_PAIR(6));
+	if (board_values[tile_nb].number == 128)
+		wattron(board[tile_nb], COLOR_PAIR(7));
+	if (board_values[tile_nb].number == 256)
+		wattron(board[tile_nb], COLOR_PAIR(8));
+	if (board_values[tile_nb].number == 512)
+		wattron(board[tile_nb], COLOR_PAIR(9));
+	if (board_values[tile_nb].number == 1024)
+		wattron(board[tile_nb], COLOR_PAIR(10));
+	if (board_values[tile_nb].number == 2048)
+		wattron(board[tile_nb], COLOR_PAIR(11));
+}
+
+void	ft_color_letters(t_block *board_values, void **board, int tile_nb)
+{
+	if (board_values[tile_nb].number == 2)
+		wattron(board[tile_nb], COLOR_PAIR(1));
+	if (board_values[tile_nb].number == 4 || board_values[tile_nb].number == 128)
+		wattron(board[tile_nb], COLOR_PAIR(2));
+	if (board_values[tile_nb].number == 8 || board_values[tile_nb].number == 256)
+		wattron(board[tile_nb], COLOR_PAIR(3));
+	if (board_values[tile_nb].number == 16 || board_values[tile_nb].number == 512)
+		wattron(board[tile_nb], COLOR_PAIR(4));
+	if (board_values[tile_nb].number == 32 || board_values[tile_nb].number == 1024)
+		wattron(board[tile_nb], COLOR_PAIR(5));
+	if (board_values[tile_nb].number == 64 || board_values[tile_nb].number == 2048)
+		wattron(board[tile_nb], COLOR_PAIR(6));
 }
 
 void	**ft_create_box(void *window, int size, t_block *board_values)
@@ -58,8 +100,10 @@ void	**ft_create_box(void *window, int size, t_block *board_values)
 		while (j < size)
 		{
 			board[tile_nb] = subwin(window, tile_height, tile_length , 1 + (j * tile_height), 2 + (i *tile_length));
+			ft_color_border(board_values, board, tile_nb);
 			wborder(board[tile_nb], 0, 0, 0, 0, 0, 0, 0, 0);
-			wattron(board[tile_nb], COLOR_PAIR(board_values[tile_nb].number));
+			ft_color_letters(board_values, board, tile_nb);
+			//wattron(board[tile_nb], COLOR_PAIR(board_values[tile_nb].number));
 			if (board_values[tile_nb].number != 0)
 				mvwprintw(board[tile_nb],
 					(tile_height / 2), (tile_length /2), "%d", board_values[tile_nb].number);
@@ -101,6 +145,22 @@ void	ft_check_size(void *window, void **board, int size)
 		;
 }
 
+void	ft_init_color(void)
+{
+	start_color();
+	init_pair(1, COLOR_WHITE, COLOR_BLACK);		//2 
+	init_pair(2, COLOR_YELLOW, COLOR_BLACK);	//4		|| 128
+	init_pair(3, COLOR_RED, COLOR_BLACK);		//8		|| 256
+	init_pair(4, COLOR_MAGENTA, COLOR_BLACK);	//16	|| 512
+	init_pair(5, COLOR_CYAN, COLOR_BLACK);		//32	|| 1024
+	init_pair(6, COLOR_GREEN, COLOR_BLACK);		//64	|| 2048
+	init_pair(7, COLOR_YELLOW, COLOR_YELLOW);	//128 border
+	init_pair(8, COLOR_RED, COLOR_RED);			//256 border
+	init_pair(9, COLOR_MAGENTA, COLOR_MAGENTA);	//512 border
+	init_pair(10, COLOR_CYAN, COLOR_CYAN);		//1024 border
+	init_pair(11, COLOR_GREEN, COLOR_GREEN);	//2048 border
+}
+
 void	ft_resize_handle(int signal)
 {
 	(void)signal;
@@ -126,19 +186,7 @@ int	main(int argc, char **argv)
 	if (board_values == NULL)
 		return (1);
 	window = initscr();
-	start_color(); //added
-	init_pair(1, COLOR_WHITE, COLOR_WHITE);
-	init_pair(2, COLOR_WHITE, COLOR_BLACK);
-	init_pair(4, COLOR_YELLOW, COLOR_BLACK);
-	init_pair(8, COLOR_RED, COLOR_BLACK);
-	init_pair(16, COLOR_MAGENTA, COLOR_BLACK);
-	init_pair(32, COLOR_CYAN, COLOR_BLACK);
-	init_pair(64, COLOR_GREEN, COLOR_BLACK);
-	init_pair(128, COLOR_BLACK, COLOR_YELLOW);
-	init_pair(256, COLOR_BLACK, COLOR_RED);
-	init_pair(512, COLOR_BLACK, COLOR_MAGENTA);
-	init_pair(1024, COLOR_BLACK, COLOR_CYAN);
-	init_pair(2048, COLOR_BLACK, COLOR_GREEN);
+	ft_init_color();
 	cbreak();
 	noecho();
 	keypad(window, TRUE);
